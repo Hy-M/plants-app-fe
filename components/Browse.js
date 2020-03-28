55a156f565c97530a3bb2e71273daabdc3400079
Hy-M <humayraa.m@outlook.com>
2020-03-28 16:14:31 +0000

    acess camera and camera roll

--- components/Browse.js
+++ components/Browse.js
@@ -1,15 +1,56 @@
-import React, { Component } from 'react';
-import { View, Text } from "react-native";
+import React, { Component } from "react";
+import { Image, View, Text, Button, Alert } from "react-native";
 import { globalStyles } from "../styles/global";
+import * as ImagePicker from "expo-image-picker";
 
 class Browse extends Component {
-    render() {
-        return (
-            <View style={globalStyles.container}>
-                <Text>Browse plants here :O</Text>
-            </View >
+  state = {
+    image: null,
+  };
+
+  componentDidMount() {
+    this.getCameraPermission();
+  }
+
+  getCameraPermission = () => {
+    ImagePicker.requestCameraPermissionsAsync().then((permissionResponse) => {
+      !permissionResponse.granted &&
+        Alert.alert(
+          "Permission required",
+          "You need to allow access to your camera and camera roll in order to identify plants.",
+          [{ text: "Okay" }]
         );
-    }
+    });
+  };
+
+  launchCamera = () => {
+    ImagePicker.launchCameraAsync().then((result) => {
+      if (!result.cancelled) {
+        this.setState({ image: result.uri });
+      }
+    });
+  };
+
+  launchLibary = () => {
+    ImagePicker.launchImageLibraryAsync().then((result) => {
+      if (!result.cancelled) {
+        this.setState({ image: result.uri });
+      }
+    });
+  };
+
+  render() {
+    let { image } = this.state;
+
+    return (
+      <View style={globalStyles.container}>
+        <Text>Take a picture to find out about a plant here :O</Text>
+        <Button title="Camera" onPress={this.launchCamera} />
+        <Button title="Camera roll" onPress={this.launchLibary} />
+        {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
+      </View>
+    );
+  }
 }
 
 export default Browse;
