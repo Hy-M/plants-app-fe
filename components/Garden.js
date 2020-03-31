dd4aa43e77a02014eb5faabbc2d782d563e16abe
Hy-M <humayraa.m@outlook.com>
2020-03-31 12:18:12 +0100

    handle date edit on modal

--- components/Garden.js
+++ components/Garden.js
@@ -11,6 +11,7 @@ import {
 } from "react-native";
 import { globalStyles } from "../styles/global";
 import * as api from "../api";
+import DatePicker from "react-native-datepicker";
 
 class Garden extends Component {
   state = {
@@ -20,6 +21,12 @@ class Garden extends Component {
     modalSinglePlantId: null,
     notesText: null,
     editsMade: null,
+    defaultImgUrl:
+      "https://images.pexels.com/photos/1072824/pexels-photo-1072824.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    datePlanted: null,
+    dateSown: null,
+    modalClosing: false,
+    loading: true,
   };
 
   componentDidMount() {
@@ -27,7 +34,8 @@ class Garden extends Component {
   }
 
   componentDidUpdate(prevProps, prevState) {
-    if (prevState.garden.length !== this.state.garden.length) {
+    let { garden } = this.state;
+    if (prevState.garden.length !== garden.length) {
       this.fetchGarden();
     } else if (this.state.editsMade) {
       this.fetchGarden();
@@ -38,7 +46,7 @@ class Garden extends Component {
     api
       .getGarden()
       .then(({ garden }) => {
-        this.setState({ garden });
+        this.setState({ garden, loading: false });
       })
       .catch((err) => console.log(err, "< err in fetchGarden"));
   };
@@ -63,12 +71,13 @@ class Garden extends Component {
   };
 
   handleModalClose = () => {
+    let { editsMade } = this.state;
     this.setState(
       (currentState) => {
-        return { modalIsVisible: !currentState.modalIsVisible };
+        return { modalIsVisible: !currentState.modalIsVisible, modalClosing: true };
       },
       () => {
-        if (this.state.editsMade) {
+        if (editsMade) {
           this.updatePlantDetails();
         }
       }
@@ -78,12 +87,13 @@ class Garden extends Component {
   updatePlantDetails = () => {
     let updatedPlantDetails = {
       notes: this.state.notesText,
+      date_planted: this.state.datePlanted,
+      date_sown: this.state.dateSown,
     };
     api
       .patchPlantDetails(updatedPlantDetails, this.state.modalSinglePlantId)
       .then((updatedPlant) => {
-        console.log(updatedPlant, "< updated platn");
-        this.setState({ editsMade: false });
+        this.setState({ editsMade: false, modalClosing: false, loading: false });
       })
       .catch((err) => console.log(err, "< err in updatePlant"));
   };
@@ -92,8 +102,26 @@ class Garden extends Component {
     this.setState({ editsMade: true, notesText: text });
   };
 
+  handleDateChange = (date, dateTitle) => {
+    if (dateTitle === "planted") {
+      this.setState({ editsMade: true, datePlanted: date });
+    } else {
+      this.setState({ editsMade: true, dateSown: date });
+    }
+  };
+
   render() {
-    const { garden, plantIsDeleting, modalIsVisible, notesText } = this.state;
+    const {
+      garden,
+      plantIsDeleting,
+      modalIsVisible,
+      notesText,
+      defaultImgUrl,
+      datePlanted,
+      dateSown,
+      loading,
+    } = this.state;
+
     return (
       <ScrollView>
         <View style={globalStyles.container}>
@@ -103,18 +131,30 @@ class Garden extends Component {
               <Text>Deleting...</Text>
             </View>
           )}
+          {loading && (
+            <View>
+              <Text>Loading...</Text>
+            </View>
+          )}
           {garden.map((plant) => {
             return (
-              <View key={plant.plant_id}>
-                <Text>{plant.name}</Text>
+              <View key={plant.plant_id} style={globalStyles.smallImagesContainer}>
+                {/* <Text style={globalStyles.smallImagesTitle}>{plant.name}</Text> */}
                 <TouchableOpacity
                   onLongPress={() => this.removePlant(plant.plant_id)}
                   onPress={() => {
                     this.handleImagePress(plant.plant_id);
-                    this.setState({ notesText: plant.notes });
+                    this.setState({
+                      notesText: plant.notes,
+                      datePlanted: plant.date_planted,
+                      dateSown: plant.date_sown,
+                    });
                   }}
                 >
-                  <Image source={{ url: plant.image_first }} style={globalStyles.imgPreview} />
+                  <Image
+                    source={{ url: plant.image_first || defaultImgUrl }}
+                    style={globalStyles.smallImages}
+                  />
                 </TouchableOpacity>
                 <Modal animationType={"slide"} transparent={false} visible={modalIsVisible}>
                   <View style={globalStyles.modal}>
@@ -125,11 +165,27 @@ class Garden extends Component {
                       onChangeText={(text) => this.handleInputChange(text)}
                       value={notesText}
                     />
-                    <Text>Planted out on: {plant.date_planted}</Text>
-                    <Text>Seeds sown on: {plant.date_sown}</Text>
+                    <Text>Planted out on:</Text>
+                    <DatePicker
+                      date={datePlanted}
+                      mode="date"
+                      format="YYYY-MM-DD"
+                      confirmBtnText="Done"
+                      cancelBtnText="Cancel"
+                      onDateChange={(date) => this.handleDateChange(date, "planted")}
+                    />
+                    <Text>Seeds sown on:</Text>
+                    <DatePicker
+                      date={dateSown}
+                      mode="date"
+                      format="YYYY-MM-DD"
+                      confirmBtnText="Done"
+                      cancelBtnText="Cancel"
+                      onDateChange={(date) => this.handleDateChange(date, "sown")}
+                    />
                     <TouchableOpacity>
                       <Button
-                        title="Close"
+                        title="Save"
                         onPress={() => {
                           this.handleModalClose();
                         }}
